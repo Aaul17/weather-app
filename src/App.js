b4d77eeb1b2f1948e261870ebff84c32d8c5e58b
Aaul17 <alex.aulum@gmail.com>
2019-07-27 12:57:15 -0700

    added state and current location

--- src/App.js
+++ src/App.js
@@ -2,25 +2,59 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {
+    city: "",
+    temp: null,
+    pressure: null,
+    humidity: null,
+    minTemp: null,
+    maxTemp: null,
+    desc: "",
+    lat: null,
+    long: null
+  }
+
+  geoSuccess = (position) => {
+    this.setState({
+      lat: position.coords.latitude,
+      long: position.coords.longitude
+    }, () => console.log(this.state.lat))
+  }
+
+  getCurrentLocation = () => {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.geoSuccess);
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  componentDidMount() {
+    this.getCurrentLocation();
+  }
+
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+      Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+      Learn React
+      </a>
       </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
